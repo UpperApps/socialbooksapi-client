ad5fc4dcd4c910b7fd0780ca7a6d5c00ff2733dd
Rodrigo Melo <rodrigomelo01@gmail.com>
2016-05-31 06:44:59 -0300

    Fim da aula 4.4 de Spring REST.

--- src/main/java/com/algaworks/socialbooks/aplicacao/Aplicacao.java
+++ src/main/java/com/algaworks/socialbooks/aplicacao/Aplicacao.java
@@ -10,7 +10,7 @@ import com.algaworks.socialbooks.domain.Livro;
 public class Aplicacao {
 	public static void main(String[] args) throws ParseException {
 		
-		LivrosClient cliente = new LivrosClient();
+		LivrosClient cliente = new LivrosClient("http://localhost:8080", "algaworks", "s3nh4");
 		
 		List<Livro> listaLivros = cliente.listar();
 		
@@ -31,5 +31,9 @@ public class Aplicacao {
 		String localizacao = cliente.salvar(livro);
 		
 		System.out.println("URI do livro salvo: " + localizacao);
+		
+		Livro livroBuscado = cliente.buscar(localizacao);
+		
+		System.out.println("Livro buscado: " + livroBuscado.getNome());
 	}
 }
--- src/main/java/com/algaworks/socialbooks/client/LivrosClient.java
+++ src/main/java/com/algaworks/socialbooks/client/LivrosClient.java
@@ -2,6 +2,7 @@ package com.algaworks.socialbooks.client;
 
 import java.net.URI;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
 
 import org.springframework.http.RequestEntity;
@@ -11,15 +12,31 @@ import org.springframework.web.client.RestTemplate;
 import com.algaworks.socialbooks.domain.Livro;
 
 public class LivrosClient {
+	
+	private RestTemplate restTemplate;
+	
+	private String URI_BASE;
+	
+	private String URN_BASE = "/livros";
+	
+	private String credencial;
+	
+	public LivrosClient(String url, String usuario, String senha){
+		restTemplate = new RestTemplate();
+		
+		URI_BASE = url.concat(URN_BASE);
+		
+		String credencialAux = usuario + ":" + senha;
+		
+		credencial = "Basic " + Base64.getEncoder().encodeToString(credencialAux.getBytes());
+	}
 
 	public List<Livro> listar() {
 
-		RestTemplate restTemplate = new RestTemplate();
-
 		// Configura o request para a o recurso /livros que retorna um array de
 		// livros.
-		RequestEntity<Void> request = RequestEntity.get(URI.create("http://localhost:8080/livros"))
-				.header("Authorization", "Basic YWxnYXdvcmtzOnMzbmg0").build();
+		RequestEntity<Void> request = RequestEntity.get(URI.create(URI_BASE))
+				.header("Authorization", credencial).build();
 
 		// O objeto restTemplate executa o request e retorna para o response o
 		// array de livors.
@@ -30,12 +47,11 @@ public class LivrosClient {
 	}
 	
 	public String salvar(Livro livro){
-		RestTemplate restTemplate = new RestTemplate();
 
 		// Configura o request para a o recurso /livros que retorna um array de
 		// livros.
-		RequestEntity<Livro> request = RequestEntity.post(URI.create("http://localhost:8080/livros"))
-				.header("Authorization", "Basic YWxnYXdvcmtzOnMzbmg0").body(livro);
+		RequestEntity<Livro> request = RequestEntity.post(URI.create(URI_BASE))
+				.header("Authorization", credencial).body(livro);
 
 		// O objeto restTemplate executa o request e retorna para o response o
 		// array de livors.
@@ -43,5 +59,16 @@ public class LivrosClient {
 
 		// Transforma o array em uma lista.
 		return response.getHeaders().getLocation().toString();
+	}
+	
+	public Livro buscar(String uri){
+		
+		RequestEntity<Void> request = RequestEntity.get(URI.create(uri))
+				.header("Authorization", credencial).build();
+		
+		ResponseEntity<Livro> response = restTemplate.exchange(request, Livro.class);
+		
+		return response.getBody();
+		
 	}
 }
